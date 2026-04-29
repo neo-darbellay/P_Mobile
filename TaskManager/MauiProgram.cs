d82b3c484162df2f4816637760989ce89d76667e
neo-darbellay <178883466+neo-darbellay@users.noreply.github.com>
2026-04-29 16:33:02 +0200

    feat(ShowList): Rendu visuel de ShowList
    [190][Done]
    Il m'a fallu installer un nouveau packet NuGet pour faire des animations "onClick", mais celui là ne voulait pas s'installer pendant un long moment.
    
    J'ai aussi eu quelques soucis à propos du ScrollView, des padding/margin et des spacing, ce qui m'a retardé énormément. Je m'occuperais des autres pages la prochaine fois.

--- TaskManager/MauiProgram.cs
+++ TaskManager/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
 
 namespace TaskManager
 {
@@ -9,6 +10,7 @@ namespace TaskManager
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("InriaSans-Bold.ttf", "InriaSansBold");
