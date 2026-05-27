c9831a057bf6419be91bd654d39311f4f2e0ba46
neo-darbellay <178883466+neo-darbellay@users.noreply.github.com>
2026-05-27 15:08:28 +0200

    feat(Commentaires): Ajout de commentaires
    [47][WIP]

--- TaskManager/Pages/NewList.xaml.cs
+++ TaskManager/Pages/NewList.xaml.cs
@@ -6,7 +6,14 @@ namespace TaskManager.Pages;
 
 public partial class NewList : ContentPage
 {
+	/// <summary>
+	/// Variable used to stock the user's chosen color
+	/// </summary>
 	private Color _userPickedColor;
+
+	/// <summary>
+	/// The list's color, chosen by the user
+	/// </summary>
 	public Color UserPickedColor
 	{
 		get => _userPickedColor;
@@ -26,25 +33,37 @@ public partial class NewList : ContentPage
 		UserPickedColor = Colors.Red;
 	}
 
-    private async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    /// <summary>
+    /// Open a pop-up to allow the user to choose a color
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private async void OpenColorPopup(object sender, TappedEventArgs e)
     {
-
+        // Create a new popup
         ColorPickerPopup popup = new(UserPickedColor);
+        Object? result = await this.ShowPopupAsync(popup);
 
-        var result = await this.ShowPopupAsync(popup);
-
+        // Verify that the result is a color, then update the chosen color
         if (result is Color color)
-        {
             UserPickedColor = color;
-        }
-
     }
 
+    /// <summary>
+    /// Undo the list creation
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private async void Cancel_Clicked(object sender, EventArgs e)
     {
 		await Shell.Current.GoToAsync("..", true);
     }
 
+    /// <summary>
+    /// Create the new list, making sure that all required fields are filled in
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void Create_Clicked(object sender, EventArgs e)
     {
 
--- TaskManager/Pages/NewTask.xaml.cs
+++ TaskManager/Pages/NewTask.xaml.cs
@@ -7,11 +7,21 @@ public partial class NewTask : ContentPage
 		InitializeComponent();
 	}
 
+    /// <summary>
+    /// Undo the task creation
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private async void Cancel_Clicked(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("..", true);
     }
 
+    /// <summary>
+    /// Create the new task, making sure that all required fields are filled in
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void Create_Clicked(object sender, EventArgs e)
     {
 
